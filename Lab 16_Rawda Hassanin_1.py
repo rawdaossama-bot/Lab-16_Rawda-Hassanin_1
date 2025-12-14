6729f2f0a98cff8d9529e0f5b91bb6c602febca3
Rawda <Rawda.Osama03@sgmail.com>
2025-12-14 15:28:14 -0500

    Used the datetime class to convert strings to dates and display exact dates on the x-axis.

--- Lab 16_Rawda Hassanin_1.py
+++ Lab 16_Rawda Hassanin_1.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import csv
 import matplotlib.pyplot as plt
+from datetime import datetime
 
 path = Path('OHUR.csv')
 lines = path.read_text(encoding="utf-8").splitlines()
@@ -13,16 +14,17 @@ for index, col_title in enumerate(header_row):
 print()
 
   
-dates = []
+Dates = []
 Rates = []
 
 for row in reader:
     try:
+        current_date = datetime.strptime(row[0], "%Y-%m-%d")
         
         Rate = float(row[1])
     except ValueError:
         print(f"Missing data for {Rate}")
     else:
-       
+        Dates.append(current_date)
         Rates.append(Rate)
         
